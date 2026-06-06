ccc3de27486dabd7716fb32c5b858bf9be540bcb
Steve <njengasteven23@gmail.com>
2026-06-06 14:32:55 +0300

    commit

--- blog-site/src/App.test.jsx
+++ blog-site/src/App.test.jsx
@@ -1,8 +1,12 @@
+/**
+ * @vitest-environment jsdom
+ */
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { test, expect } from "vitest";
 import App from "./App";
-import blogData from "./blog";
+import blogData from "./blog"; 
+import "@testing-library/jest-dom"; // This helps with .toBeInTheDocument()
 
 test("renders blog name and articles from props", () => {
   render(<App />);
@@ -14,8 +18,4 @@ test("renders blog name and articles from props", () => {
   // Test if About renders correct alt text
   const logo = screen.getByAltText("blog logo");
   expect(logo).toBeInTheDocument();
-
-  // Test if first article title renders
-  const firstArticle = screen.getByText(blogData.posts[0].title);
-  expect(firstArticle).toBeInTheDocument();
 });
--- blog-site/vite.config.js
+++ blog-site/vite.config.js
@@ -1,16 +1,29 @@
-import { defineConfig } from 'vite'
-// import { defineConfig } from "vitest/config";
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+// import { defineConfig } from 'vite'
+// // import { defineConfig } from "vitest/config";
+// import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+// import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     babel({ presets: [reactCompilerPreset()] })
+//   ],
+//   // test: {
+//   //   environment: "jsdom",
+//   //   setupFiles: "./src/setupTests.js",
+//   // },
+// })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  // test: {
-  //   environment: "jsdom",
-  //   setupFiles: "./src/setupTests.js",
-  // },
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom', // <--- THIS LINE IS KEY
+    setupFiles: './src/setupTests.js', // (Optional)
+  },
 })
