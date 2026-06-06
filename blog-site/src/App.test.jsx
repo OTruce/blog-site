7ddbee55d53a32e61530fa7478a7851a3b826971
Steve <njengasteven23@gmail.com>
2026-06-06 13:44:00 +0300

    test file commit

---
+++ blog-site/src/App.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import blog from "./blog";
+
+test("renders blog name from props", () => {
+  render(<App />);
+
+  const title = screen.getByText(blog.name);
+  expect(title).toBeInTheDocument();
+});
